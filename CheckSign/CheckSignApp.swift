6a853b2972314eb40bc870dc85825061fbded711
jackgerraughty <jgerraughty@yahoo.com>
2025-04-15 18:13:40 -0700

    Initial Commit

---
+++ CheckSign/CheckSignApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckSignApp.swift
+//  CheckSign
+//
+//  Created by Jack Gerraughty on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct CheckSignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
